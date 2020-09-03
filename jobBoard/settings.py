1f3202ce87dc3501466fc438e2f78cb3c6ce125b
hosam00 <hosamgamal33345@gmail.com>
2020-09-03 14:08:58 +0200

    migrate

--- jobBoard/settings.py
+++ jobBoard/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # My apps
+    'home',
+    'job',
+    'contact',
+    'blog',
+    'accounts'
 ]
 
 MIDDLEWARE = [
