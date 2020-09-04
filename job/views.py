e4deb06060bd81b947a67f34937d734a2197bb40
hosam00 <hosamgamal33345@gmail.com>
2020-09-04 11:02:18 +0200

    Add front-end

--- job/views.py
+++ job/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Job
 
-# Create your views here.
+def job_list(request):
+    jobs = Job.objects.all()
+    # context = {'jobs':jobs}
+    return render(request, 'job_list.html', {'jobs':jobs})
+
+def job_detail(request, id):
+    job = Job.objects.get(id=id)
+    context = {'job':job}
+    return render(request, 'job_detail.html', context=context)
